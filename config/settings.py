929dc9121470464fa7fe2ca04143f00566ceee78
Josh596 <joshuaolukotun3@gmail.com>
2021-10-16 19:55:40 +0100

    Changed Allow Hosts

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '^uqcxa93=4z*3u=j&8o@_#%+u4%^@oukd*q#+velmw-zt(o-%+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shopbeta.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
